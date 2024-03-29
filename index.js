8cf5d4bdc828af275a01a690d78faffaaac14bf9
Anujjain-git <rrrh8424@gmail.com>
2024-03-29 11:35:49 +0530

    u30

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const cors = require('cors')
 const port = 3001
 const path = require('path');
 const mongoDB = require("./db");
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 mongoDB();
+app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -17,24 +19,12 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get("/static/js/main.f0a9c315.js", (req, res)=>{
-    // app.use("/static/js", express.static(path.resolve(__dirname, "rectpart", "build", "static", "js")));
-    console.log('sended');
-    res.sendFile(path.resolve(__dirname, "reactpart", "build", "static", "js", "main.f0a9c315.js"));
-})
-
-app.get("/static/css/main.3bae470d.css", (req, res)=>{
-    // app.use("/static/css", express.static(path.resolve(__dirname, "rectpart", "build", "static", "css")));
-    res.sendFile(path.resolve(__dirname, "reactpart", "build", "static", "css", "main.3bae470d.css"));
-})
 
 
 
-app.get('/', (req, res)=>{
+app.get("/", function (req, res) {
     app.use(express.static(path.resolve(__dirname, "reactpart", "build")));
-    res.sendFile(path.resolve(__dirname, "reactpart", "build", 'index.html'))
-    // console.log('sending file');
-    // console.log(path.resolve(__dirname, "reactpart", "build", 'index.'))
+    res.sendFile((path.join(__dirname, "/reactpart/build/index.html")));
 })
 // app.get("/", function (req, res) {
 //     res.send("Hello")
@@ -46,6 +36,6 @@ app.use('/api', require('./Routes/DisplayData'));
 
 app.use('/api', require('./Routes/OrderData'));
 
-app.listen(3001, function (req, res) {
+app.listen(process.env.PORT || 3001, function (req, res) {
     console.log("Server up at " + port);
 }) 
